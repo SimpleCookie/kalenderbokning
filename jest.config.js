03b0b697851e85a18e8d6ba7a8ade665d15050d8
Marcus <Marcus.Ekstrom@devgroup.se>
2021-07-11 21:36:15 +0200

    Add jest transform

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
   moduleNameMapper: {
     "@db": "<rootDir>/src/storage/db.ts",
     "@src/(.*)": "<rootDir>/src/$1",
