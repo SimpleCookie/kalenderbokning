65fccf4ba5fc39bdc6f836c8ada3afc18a43eb70
Marcus <Marcus.Ekstrom@devgroup.se>
2021-07-11 22:14:26 +0200

    .

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   verbose: true,
+  roots: ["<rootDir>"],
+  moduleDirectories: ["node_modules", "src"],
+  modulePaths: ["src"],
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
   moduleNameMapper: {
-    "@db": "<rootDir>/src/storage/db.ts",
-    "@src/(.*)": "<rootDir>/src/$1",
+    "^@db": "<rootDir>/src/storage/db.ts",
+    "^@src/(.*)": "<rootDir>/src/$1",
   },
 };
